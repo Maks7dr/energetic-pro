cdf06f9dc8116e2524a61f2bbcf48e947440065a
Maksym Drachenko <maks7d@gmail.com>
2025-04-02 14:34:49 +0500

    add foto swiper

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,3 @@
-// var swiper = new Swiper(".swiper-container", {
-//   loop: true,
-//   slidesPerView: 2,
-//   spaceBetween: 10,
-//   slidesPerGroup: 1,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
-
 var swiper = new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 2,
@@ -29,7 +14,9 @@ var swiper = new Swiper(".swiper-container", {
   breakpoints: {
     375: {
       slidesPerView: 1,
-      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
     },
   },
 });
