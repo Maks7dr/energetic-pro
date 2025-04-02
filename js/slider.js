f65e20b202ebcb0af02dc213fa517963ab5c8e41
Maksym Drachenko <maks7d@gmail.com>
2025-04-02 13:55:36 +0500

    add mobile

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,18 @@
+// var swiper = new Swiper(".swiper-container", {
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 10,
+//   slidesPerGroup: 1,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
 var swiper = new Swiper(".swiper-container", {
   loop: true,
   slidesPerView: 2,
@@ -10,5 +25,11 @@ var swiper = new Swiper(".swiper-container", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
   },
 });
